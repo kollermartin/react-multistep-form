9a808c0b06c8a4244cad2450c655023786ac7bff
Martin Koller <kollermartin00@gmail.com>
2022-10-04 20:17:08 +0200

    finished app

---
+++ src/App.tsx
@@ -0,0 +1,90 @@
+import { UseMultistepForm } from './useMultistepForm';
+import { UserForm } from './UserForm';
+import { AddressForm } from './AddressForm';
+import { AccountForm } from './AccountForm';
+import { FormEvent, useState } from 'react';
+
+type FormData = {
+  firstName: string;
+  lastName: string;
+  age: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  email: string;
+  password: string;
+};
+
+const INITIAL_DATA: FormData = {
+  firstName: '',
+  lastName: '',
+  age: '',
+  street: '',
+  city: '',
+  state: '',
+  zip: '',
+  email: '',
+  password: '',
+};
+
+function App() {
+  const [data, setData] = useState(INITIAL_DATA);
+
+  function updateFields(fields: Partial<FormData>) {
+    setData((prev) => {
+      return { ...prev, ...fields };
+    });
+  }
+  const { steps, currentStepIndex, step, isFirstStep, back, next, isLastStep } =
+    UseMultistepForm([
+      <UserForm {...data} updateFields = {updateFields} />,
+      <AddressForm {...data} updateFields={updateFields} />,
+      <AccountForm {...data} updateFields={updateFields} />,
+    ]);
+
+  function onSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (!isLastStep) return next();
+    alert('Successful Account creation')
+  }
+
+  return (
+    <div
+      style={{
+        position: 'relative',
+        background: 'white',
+        border: '1px solid black',
+        padding: '2rem',
+        margin: '1rem auto',
+        borderRadius: '.5rem',
+        fontFamily: 'Arial',
+        maxWidth: 'max-content',
+      }}
+    >
+      <form onSubmit={onSubmit}>
+        <div style={{ position: 'absolute', top: '.5rem', right: '.5rem' }}>
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div
+          style={{
+            marginTop: '1rem',
+            display: 'flex',
+            gap: '.5rem',
+            justifyContent: 'flex-end',
+          }}
+        >
+          {!isFirstStep && (
+            <button type="button" onClick={back}>
+              Back
+            </button>
+          )}
+          <button type="submit">{isLastStep ? 'Finish' : 'Next'}</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
